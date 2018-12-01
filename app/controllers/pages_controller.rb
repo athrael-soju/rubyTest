bc872b29fe89ee49275658a575a4d0282ad4bad7
Athrael Soju <athrael.soju@gmail.com>
2018-12-01 22:53:59 +0000

    Refactoring and library changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
   end
 
   def home
-    update()
+    update
   end
 
   def contact
@@ -13,14 +13,12 @@ class PagesController < ApplicationController
 
   def update
     doc = Nokogiri::XML(open("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"))
-
+    time_now = DateTime.now.strftime('%a, %d %b %Y %H:%M:%S')
     doc.xpath("//xmlns:Cube/xmlns:Cube/xmlns:Cube").each do |row|
       currency = row.attribute('currency')
       rate = row.attribute('rate')
-      timeNow = DateTime.now.strftime('%a, %d %b %Y %H:%M:%S')
-
-      sql = "UPDATE forexes SET rate=#{rate}, updated_at='#{timeNow}' WHERE currency ='#{currency}'"
-      ActiveRecord::Base.connection.execute(sql)
+      update_forex = "UPDATE forexes SET rate=#{rate}, updated_at='#{time_now}' WHERE currency ='#{currency}'"
+      ActiveRecord::Base.connection.execute(update_forex)
     end
   end
 end
