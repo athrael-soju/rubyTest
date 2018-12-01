dd128c67d584b489191bd899f4a6dc4097e2d564
Athrael Soju <athrael.soju@gmail.com>
2018-12-01 21:35:16 +0000

    Implement function to make API call and update database with latest Forex rates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,26 @@
+require 'nokogiri'
+
 class PagesController < ApplicationController
   def about
   end
 
   def home
-
+    update()
   end
 
   def contact
+  end
+
+  def update
+    doc = Nokogiri::XML(open("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"))
+
+    doc.xpath("//xmlns:Cube/xmlns:Cube/xmlns:Cube").each do |row|
+      currency = row.attribute('currency')
+      rate = row.attribute('rate')
+      timeNow = DateTime.now.strftime('%a, %d %b %Y %H:%M:%S')
+
+      sql = "UPDATE forexes SET rate=#{rate}, updated_at='#{timeNow}' WHERE currency ='#{currency}'"
+      ActiveRecord::Base.connection.execute(sql)
+    end
   end
 end
