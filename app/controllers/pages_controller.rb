560ca2019d20f17b2866822417895decb735a9b0
Athrael Soju <athrael.soju@gmail.com>
2018-12-04 19:57:11 +0000

    Implement date functions, refactoring, removal of unnecessary views.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,29 +1,51 @@
+include ActionView::Helpers::NumberHelper
 
 class PagesController < ApplicationController
-  def about
-  end
-
   def home
-      @option_list = ''
-      select_currencies ="select currency from forexes"
-      currencies = ActiveRecord::Base.connection.execute(select_currencies)
-      currencies.each do |row|
-      @option_list+= '<option>'+row['currency']+'</option><br/>'
-      end
-
+      init_on_load
       if params[:formControlSelect1]
         calculate_exchange
       end
   end
 
-  def calculate_exchange
-    @cur1 = params[:formControlSelect1]
-    @cur2 = params[:formControlSelect2]
-    @num = params[:number_input]
-    @cool_value = 20*30
-    @message = "#{@num} of #{@cur1} equals #{@cool_value} #{@cur2}"
+  def init_on_load
+    generate_currency_collection
+    calculate_dates
   end
 
-  def contact
+  def calculate_dates
+    select_date ="select updated_at from forexes where currency = 'USD'"
+    get_date = ActiveRecord::Base.connection.execute(select_date)[0]['updated_at']
+    @date_now = "Current:             " + DateTime.now.strftime('%a, %d %b %Y %H:%M:%S')
+    @last_updated = "Rates updated:   #{get_date}"
+
+    difference_in_dates = (DateTime.now - Date.parse(get_date)).to_i
+    if difference_in_dates > 0
+      @last_updated+= "(#{difference_in_dates} days behind. Consider updating rates.)"
+    end
+  end
+
+  def generate_currency_collection
+    @option_list = ''
+    select_currencies ="select currency from forexes"
+    currencies = ActiveRecord::Base.connection.execute(select_currencies)
+
+    currencies.each do |row|
+      @option_list+= '<option>'+row['currency']+'</option><br/>'
+    end
+  end
+
+  def calculate_exchange
+    cur1 = params[:formControlSelect1]
+    cur2 = params[:formControlSelect2]
+    quantity = params[:number_input].to_f
+    select_rate1 ="select rate from forexes where currency = '#{cur1}'"
+    select_rate2 ="select rate from forexes where currency = '#{cur2}'"
+
+    rate1 = ActiveRecord::Base.connection.execute(select_rate1)[0]['rate']
+    rate2 = ActiveRecord::Base.connection.execute(select_rate2)[0]['rate']
+    formatted_conversion = number_to_human(((1/rate1)*rate2*quantity))
+    formatted_quantity = number_to_human(params[:number_input].to_f)
+    @conversion_result = "#{formatted_quantity} #{cur1} is equivalent to: #{formatted_conversion} #{cur2}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'pages/about'
   get 'pages/home'
-  get 'pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get  'get_rates' => 'forexes#get_rates', :as => 'get_rates'
   get  'calculate_exchange' => 'pages#calculate_exchange', :as => 'calculate_exchange'
